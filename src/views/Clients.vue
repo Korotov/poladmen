5c772a4d36e2a7b7262d5302b835e8bb8f4ee5b3
Vadim Korotov <a@mvn.by>
2020-05-20 18:55:29 +0300

    Cliants view added

---
+++ src/views/Clients.vue
@@ -0,0 +1,18 @@
+<template>
+  <clients-data-table />
+</template>
+
+<script>
+// @ is an alias to /src
+import ClientsDataTable from '@/components/models/ClientsDataTable.vue'
+
+export default {
+  name: 'clients',
+  components: {
+    ClientsDataTable
+  },
+  created() {
+    this.$store.dispatch('clients/getAllClients')
+  }
+}
+</script>
